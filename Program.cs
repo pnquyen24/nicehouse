2275beb1c9e729c8a7d9b58fb210bfd2b76bbab0
flowma.24 <pnquyen.nvla2.2019@gmail.com>
2024-09-30 09:53:40 +0700

    QuyenPN-Update swagger

--- Program.cs
+++ Program.cs
@@ -21,12 +21,8 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddAutoMapper(typeof(Program));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
