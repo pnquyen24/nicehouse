01b26cf0f421c2eadf4db0e987f4e7750cc65756
flowma.24 <pnquyen.nvla2.2019@gmail.com>
2024-09-30 22:55:16 +0700

    QuyenPN-Update env

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddAutoMapper(typeof(Program));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
